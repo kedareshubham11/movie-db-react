7b019463448868ac6779e8fc130976b256b03139
kedareshubham11 <kedareshubham11@gmail.com>
2021-04-03 23:34:55 +0530

    created navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Navbar from "./Components/NavBar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Navbar />
     </div>
   );
 }
---
+++ src/Components/NavBar/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <h2>Trending</h2>
+      <h2>Top Rated</h2>
+      <h2>Action</h2>
+      <h2>Comedy</h2>
+      <h2>Horror</h2>
+      <h2>Romantic</h2>
+      <h2>Top Rated</h2>
+      <h2>Action</h2>
+    </div>
+  );
+}
+
+export default Navbar;
